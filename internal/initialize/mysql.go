94fe6fc365223dcdeb73f0130b60b86dc5129bcd
Huỳnh Chí Bảo <baohc110902@gmail.com>
2025-01-06 04:07:46 +0700

    section 22: config docker file level 2.

--- internal/initialize/mysql.go
+++ internal/initialize/mysql.go
@@ -8,6 +8,7 @@ import (
 
 	"go.uber.org/zap"
 	"gorm.io/driver/mysql"
+	"gorm.io/gen"
 	"gorm.io/gorm"
 )
 
@@ -37,6 +38,7 @@ func InitMySQL() {
 	SetPool()
 	// Migrate tables
 	migrateTables()
+	// genTableDAO()
 }
 
 func SetPool() {
@@ -59,4 +61,24 @@ func migrateTables() {
 	if err != nil {
 		fmt.Printf("Failed to migrate tables %s", err)
 	}
+}
+
+func genTableDAO() {
+	g := gen.NewGenerator(gen.Config{
+		OutPath: "./internal/model",                                                 // output path
+		Mode:    gen.WithoutContext | gen.WithDefaultQuery | gen.WithQueryInterface, // generate mode
+	})
+
+	// gormdb, _ := gorm.Open(mysql.Open("root:@(127.0.0.1:3306)/demo?charset=utf8mb4&parseTime=True&loc=Local"))
+	g.UseDB(global.Mdb) // reuse your gorm db
+	// g.GenerateAllTable() // generate all tables
+	// g.GenerateModel("go_crm_user") // generate table `go_crm_user`
+	// Generate basic type-safe DAO API for struct `model.User` following conventions
+	// g.ApplyBasic(model.User{})
+
+	// // Generate Type Safe API with Dynamic SQL defined on Querier interface for `model.User` and `model.Company`
+	// g.ApplyInterface(func(Querier) {}, model.User{}, model.Company{})
+
+	// Generate the code
+	g.Execute()
 }
